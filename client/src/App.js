0f5df9081e229fe33cc7e77dd190a9f70946db3f
Pawan Chaudhary <chaudharypawan18@yahoo.com>
2025-05-11 09:44:31 +0530

    work is done man

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "antd";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello man what are you doing</h1>
+      <Button type="primary">Antd Button</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { ConfigProvider } from "antd";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <ConfigProvider
+      theme={{
+        components: {
+          Button: {
+            colorPrimary: "#40513B",
+            colorPrimaryHover: "#40513B",
+            borderRadius: "0px",
+          },
+        },
+      }}
+    >
+      <App />
+    </ConfigProvider>
+  </React.StrictMode>
+);
